4f9a84bfa3a20f99cf7b86ba0f72d9bac6fc85cb
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-11-12 00:15:18 -0600

    cambio de diseño- momentaneamente
    se agrego la vista desde ese modulo

--- views/categorias.php
+++ views/categorias.php
@@ -10,15 +10,24 @@
 <a class="catelectronico" href="productosCategoria.php?categoria=<?php echo 'ENFERMERIA'?>">
 ENFERMERIA
 </a>
+<a class="pdf" href="../includes/reporte_cat.php?categoria=<?php echo'ENFERMERIA'?>">
+VER PDF- ENFERMERIA
+</a>
         </div>
 <div class="col-sm-4">
 <a class="catcocina" href="productosCategoria.php?categoria=<?php echo 'GASTRONOMIA'?>">
 GASTRONOMIA
 </a>
+<a class="pdf" href="../includes/reporte_cat.php?categoria=<?php echo'GASTRONOMIA'?>">
+VER PDF - GASTRONOMIA
+</a>
 </div>  
 <div class="col-sm-4">
 <a class="catfarmaceutico" href="productosCategoria.php?categoria=<?php echo 'AGROINDUSTRIAL' ?>">
 AGROINDUSTRIAL
+</a>
+<a class="pdf" href="../includes/reporte_cat.php?categoria=<?php echo'AGROINDUSTRIAL'?>">
+VER PDF - AGROINDUSTRIAL
 </a>
 </div>  
 </div>
@@ -27,14 +36,23 @@ AGROINDUSTRIAL
 <a class="catjugueteria" href="productosCategoria.php?categoria=<?php echo 'BIOTECNOLOGIA'?>">
 BIOTECNOLOGIA
 </a>
+<a class="pdf" href="../includes/reporte_cat.php?categoria=<?php echo'BIOTECNOLOGIA'?>">
+VER PDF - BIOTECNOLOGIA
+</a>
 </div>
 
 <div class="col-sm-4">
 <a class="catmascotas" href="productosCategoria.php?categoria=<?php echo'VETERINARIA'?>">
 VETERINARIA
 </a>
+<a class="pdf" href="../includes/reporte_cat.php?categoria=<?php echo'VETERINARIA'?>">
+VER PDF - VETERINARIA
+</a>
 </div>
 <div class="col-sm-4">
+<a class="catautomovilstico" href="productosCategoria.php?categoria=<?php ?>">
+...
+</a>
 <a class="catautomovilstico" href="productosCategoria.php?categoria=<?php ?>">
 ...
 </a>
