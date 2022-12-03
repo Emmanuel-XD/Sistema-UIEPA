7990ac008e89748e8d514c486322fd4838baf800
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-12-03 10:43:28 -0600

    se quitaron scripts

--- includes/footer.php
+++ includes/footer.php
@@ -8,8 +8,6 @@
     <!-- Custom scripts for all pages-->
     <script src="../js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
-    <script src="../vendor/chart.js/Chart.min.js"></script>
 
         <!-- Page level plugins -->
     <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
@@ -17,6 +15,3 @@
     <!-- Page level custom scripts -->
     <script src="../js/demo/datatables-demo.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="../js/demo/chart-area-demo.js"></script>
-    <script src="../js/demo/chart-pie-demo.js"></script>
