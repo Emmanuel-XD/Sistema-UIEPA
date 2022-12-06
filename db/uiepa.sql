a95a9b22f37a79037f9957268774f996f188758c
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-12-06 09:28:20 -0600

    nuevo campo

--- db/uiepa.sql
+++ db/uiepa.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-11-2022 a las 22:51:21
+-- Tiempo de generación: 06-12-2022 a las 16:25:08
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.8
 
@@ -54,24 +54,32 @@ CREATE TABLE `insumo` (
   `id` int(11) NOT NULL,
   `fecha_entrada` date NOT NULL,
   `cantidad` int(50) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
   `unidad` varchar(150) NOT NULL,
   `nombre` varchar(150) NOT NULL,
   `fecha_caducidad` date NOT NULL,
   `marca` varchar(150) NOT NULL,
   `existencia` varchar(150) NOT NULL,
   `categoria` varchar(50) NOT NULL,
-  `entregado` int(10) NOT NULL
+  `entregado` int(10) NOT NULL,
+  `recibido` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `insumo`
 --
 
-INSERT INTO `insumo` (`id`, `fecha_entrada`, `cantidad`, `unidad`, `nombre`, `fecha_caducidad`, `marca`, `existencia`, `categoria`, `entregado`) VALUES
-(1, '2022-11-11', 100, 'PZA', 'usuario', '2022-11-26', 'Prueba', '0', 'GASTRONOMIA', 10),
-(3, '2022-11-17', 100, 'PZAss', 'Isnumo', '2022-11-24', 'Prueba', '5', 'VETERINARIA', 5),
-(4, '2022-11-15', 200, 'PZA', 'Material', '2022-12-01', 'FERMONT', '5', 'VETERINARIA', 5),
-(8, '2022-11-13', 1, 'Litros', 'test', '2022-12-07', 'a2', '5', 'ENFERMERIA', 5);
+INSERT INTO `insumo` (`id`, `fecha_entrada`, `cantidad`, `tipo`, `unidad`, `nombre`, `fecha_caducidad`, `marca`, `existencia`, `categoria`, `entregado`, `recibido`) VALUES
+(4, '2022-11-15', 10, 'Consumibles medicos', 'Litros', 'Material', '2022-12-01', 'FERMONT', '8', 'VETERINARIA', 2, 200),
+(8, '2022-11-13', 10, 'Farmacos', 'Litros', 'test', '2022-12-07', 'a2', '6', 'VETERINARIA', 2, 0),
+(12, '2022-11-16', 15, 'Insumos', 'Piezas', 'fff', '2022-11-10', 'Prueba', '0', 'GASTRONOMIA', 15, 5),
+(15, '2022-11-10', 30, 'Farmacos', 'Piezas', 'jhfhgj', '2022-11-10', 'FERMONT', '5', 'AGROINDUSTRIAL', 5, 100),
+(16, '2022-11-24', 20, 'Consumibles laboratorio', 'Piezas', 'gfhjfjg', '2022-11-09', 'Prueba', '5', 'BIOTECNOLOGIA', 5, 50),
+(21, '2022-11-10', 20, 'Farmacos', 'Mililitros', 'estrest', '2022-11-04', 'Prueba', '5', 'VETERINARIA', 5, 0),
+(22, '2022-12-08', 10, 'Consumibles medicos', 'Mililitros', 'fbcfg', '2022-12-13', '20', '5', 'VETERINARIA', 5, 0),
+(26, '2022-12-01', 25, 'Insumos', 'Metros', 'Alumno', '2022-12-20', 'dfgfdh', '5', 'ENFERMERIA', 15, 5),
+(30, '2022-12-06', 20, 'Reactivos', 'Litros', 'user', '2022-12-15', 'Prueba', '5', 'ENFERMERIA', 5, 0),
+(31, '2022-12-21', 10, 'Consumibles laboratorio', 'Paquetes', 'vdfvbgfdx', '2022-12-14', 'FERMONT', '5', 'ENFERMERIA', 5, 200);
 
 -- --------------------------------------------------------
 
@@ -113,8 +121,7 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `username`, `password`, `fecha`, `rol_id`) VALUES
 (26, 'Emanuel', 'Emanuel', '$2y$05$HS5IA8LOcQDTnlgvb7ojGeMBlAb7NGtW33MNXjL2llPoacPBIIMEW', '2022-11-16 06:11:26', 1),
-(28, 'prueba', 'prueba', '$2y$05$eL3KnZJ65hzAYVmZR.8efeTsf2w8GWUEe4XAsa9NpVujCbeuuABc.', '2022-11-16 22:46:30', 2),
-(29, 'usuario', 'usuario', '$2y$05$m0O4XSo4Ik1GJktnOSp6punWXhTVuZt86Ri6xbaOCwppAbUxypXmW', '2022-11-16 23:01:39', 2);
+(30, 'usuario', 'usuario', '$2y$05$Rz5brhBVP7YlFs5x5klgTOYLYqHOyH6rqfMZasG6sKAI5giTT4TXe', '2022-12-02 06:04:15', 2);
 
 --
 -- Índices para tablas volcadas
@@ -160,7 +167,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `insumo`
 --
 ALTER TABLE `insumo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `permisos`
@@ -172,7 +179,7 @@ ALTER TABLE `permisos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Restricciones para tablas volcadas
