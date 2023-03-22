c4161bf674bb10006dfff84d2c1aedc7523fb20f
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-03-22 00:28:43 -0600

    Add files via upload

--- views/categorias.php
+++ views/categorias.php
@@ -9,11 +9,11 @@
     <div class="row">
         <div class="col-sm-4  justify-content-center">
 <a class="ENFERMERIA" href="enfermeria.php?categoria=<?php echo 'ENFERMERIA'?>">
-<img class="img-style"  src="../img/enfer.png" width="130px" height="120px" alt="">
+<img class="img-style"  src="../img/enfer.jpg" width="130px" height="120px" alt="">
 ENFERMERIA
 </a>
 <a class="pdf" href="../includes/reportes/reporte_enfermeria.php?categoria=<?php echo'ENFERMERIA'?>">
-VER PDF- ENFERMERIA
+<img src="../img/pdf.png" width="50px" height="40px" alt="">
 </a>
         </div>
 <div class="col-sm-4 adjuster justify-content-center">
@@ -22,16 +22,16 @@ VER PDF- ENFERMERIA
 GASTRONOMIA
 </a>
 <a class="pdf" href="../includes/reportes/reporte_gastronomia.php?categoria=<?php echo'GASTRONOMIA'?>">
-VER PDF - GASTRONOMIA
+<img src="../img/pdf.png" width="50px" height="40px" alt="">
 </a>
 </div>  
 <div class="col-sm-4 adjuster justify-content-center">
 <a class="AGROINDUSTRIAL" href="agroindustrial.php?categoria=<?php echo 'AGROINDUSTRIAL' ?>">
-<img  class="img-style" src="../img/agroindustrial.jpg" width="130px" height="120px" alt="">
+<img  class="img-style" src="../img/agro.jpg" width="130px" height="120px" alt="">
 AGROINDUSTRIAL
 </a>
 <a class="pdf" href="../includes/reportes/reporte_agroindustrial.php?categoria=<?php echo'AGROINDUSTRIAL'?>">
-VER PDF - AGROINDUSTRIAL
+<img src="../img/pdf.png" width="50px" height="40px" alt="">
 </a>
 </div>  
 </div>
@@ -39,32 +39,34 @@ VER PDF - AGROINDUSTRIAL
 <div class="row">
 <div class="col-sm-4 adjuster justify-content-center">
 <a class="BIOTECNOLOGIA" href="biotecnologia.php?categoria=<?php echo 'BIOTECNOLOGIA'?>">
-<img class="img-style" src="../img/agroindustrial.jpg" width="130px" height="120px" alt="">
+<img class="img-style" src="../img/bio.jpg" width="130px" height="120px" alt="">
 BIOTECNOLOGIA
 </a>
 <a class="pdf" href="../includes/reportes/reporte_biotecnologia.php?categoria=<?php echo'BIOTECNOLOGIA'?>">
-VER PDF - BIOTECNOLOGIA
+<img src="../img/pdf.png" width="50px" height="40px" alt="">
 </a>
 </div>
 
 <div class="col-sm-4 adjuster justify-content-center">
 <a class="VETERINARIA" href="veterinaria.php?categoria=<?php echo'VETERINARIA'?>">
-<img  class="img-style" src="../img/veterinaria.webp" width="130px" height="120px" alt="">
+<img  class="img-style" src="../img/veterinaria.jpg" width="130px" height="120px" alt="">
 VETERINARIA
 </a>
 <a class="pdf" href="../includes/reportes/reporte_veterinaria.php?categoria=<?php echo'VETERINARIA'?>">
-VER PDF - VETERINARIA
+<img src="../img/pdf.png" width="50px" height="40px" alt="">
 </a>
 </div>
 
 </div>
 </div>
 </center>
+<!-- 
 <div class="row">
     <div class="col-sm-12">
         <input class="soon" type="button" value="Mas categorias proximamenente">
     </div>
 </div>
+-->
 
 </body>
 <?php require '../includes/footer.php'; ?>
